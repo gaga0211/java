b23d30bec6f8b74ea4e11f72305921481a0f444c
嘎嘎 <gaga@MacBook-Pro.lan>
2018-12-03 19:38:38 +0800

    sqrt方法

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,9 @@ public class HelloWorld
 {
   public static void main(String[] args) {
     System.out.println("hello \nworld");
+    System.out.println(2.0 - 1.1);
+    double x = 4;
+    double y = Math.sqrt(x);
+    System.out.println(y);
   }
 }
