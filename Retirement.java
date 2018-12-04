00264ee8ea42f7ae77e718883d07fdf76d897c00
嘎嘎 <gaga@MacBook-Pro.lan>
2018-12-04 20:54:44 +0800

    Retirement

---
+++ Retirement.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Retirement{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("How much money do you need to retire?");
+		double goal = in.nextDouble();
+
+		System.out.print("How much will you contribute every year?");
+		double payment = in.nextDouble();
+
+		System.out.print("Interest rate in %: ");
+		double interestRate = in.nextDouble();
+		
+		double balance = 0;
+		int years = 0;
+		while (balance < goal){
+			balance += payment;
+			double Interest = balance * interestRate / 100;
+			balance += Interest;
+			years++;
+		} 
+		System.out.println("You can retire in " + years + " years.");
+  }
+}
